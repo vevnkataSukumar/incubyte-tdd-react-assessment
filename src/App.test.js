c529f49408feaf28de2e36b32ba1682d53d8a02a
venkata sukumar <venkatasukumar@venkatas-MacBook-Pro.local>
2025-01-22 22:41:34 +0530

    Failed case without any component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  test('should add numbers and display the sum', () => {
+    render(<App />);
+
+    // Initially, the sum should be 0
+    const sumElement = screen.getByTestId('sum');
+    expect(sumElement).toHaveTextContent('0');
+
+  });
 });
